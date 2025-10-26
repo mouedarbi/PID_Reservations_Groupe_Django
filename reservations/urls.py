bfbb2db64f99d6f72715146fe856765465f3532c
Mohamed Ouedarbi <mouedarbi@gmail.com>
2025-10-26 22:03:19 +0100

    Frontend authentication – Basic signup

--- reservations/urls.py
+++ reservations/urls.py
@@ -23,6 +23,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     #path('', RedirectView.as_view(url='/artist/', permanent=False)),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('catalogue/', include('catalogue.urls')),
