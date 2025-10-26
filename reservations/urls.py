4867d9475a528804c9e1cfd1fbefdf3ae724c474
Mohamed Ouedarbi <mouedarbi@gmail.com>
2025-10-26 20:09:56 +0100

    Django Admin configuration

--- reservations/urls.py
+++ reservations/urls.py
@@ -23,3 +23,7 @@ urlpatterns = [
     path('catalogue/', include('catalogue.urls')),
     path('admin/', admin.site.urls),
 ]
+
+admin.site.index_title = "Projet Réservations"
+admin.site.index_header = "Projet Réservations HEADER"
+admin.site.site_title = "Spectacles"
