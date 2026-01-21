ff2d64241774d484bd6b14bdd888a6b22724cd99
Mohamed Ouedarbi <mouedarbi@gmail.com>
2026-01-21 13:39:58 +0100

    API: Implémentation du sérialiseur et des vues pour le modèle Price

--- api/views/prices.py
+++ api/views/prices.py
@@ -1,28 +1,51 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+from django.http import Http404
+
+from catalogue.models.price import Price
+from ..serializers.prices import PriceSerializer
 
 class PricesView(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def post(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def put(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def delete(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
+    """
+    List all prices, or create a new price.
+    """
+    def get(self, request, format=None):
+        prices = Price.objects.all()
+        serializer = PriceSerializer(prices, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PriceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PricesDetailView(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def post(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def put(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
-    
-    def delete(self, request, *args, **kwargs):
-        return Response({"detail": "Placeholder"}, status=501)
+    """
+    Retrieve, update or delete a price instance.
+    """
+    def get_object(self, id):
+        try:
+            return Price.objects.get(id=id)
+        except Price.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id, format=None):
+        price = self.get_object(id)
+        serializer = PriceSerializer(price)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        price = self.get_object(id)
+        serializer = PriceSerializer(price, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id, format=None):
+        price = self.get_object(id)
+        price.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
