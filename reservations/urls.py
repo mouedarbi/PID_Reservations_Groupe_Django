cb2817b5e2d364485a7ffcecba238ff1c281a7f1
Mohamed Ouedarbi <mouedarbi@gmail.com>
2026-01-26 18:58:23 +0100

    feat(frontend): intégration de l'authentification et mise à jour du menu de navigation

--- reservations/urls.py
+++ reservations/urls.py
@@ -21,6 +21,8 @@ from django.urls import include, path
 urlpatterns = [
     #path('', RedirectView.as_view(url='/artist/', permanent=False)),
     path('', include('frontend.urls')), # Added for frontend app
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('catalogue/', include('catalogue.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
