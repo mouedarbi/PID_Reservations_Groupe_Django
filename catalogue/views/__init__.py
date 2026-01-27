0c2a2c6361b969698a8c8091b65b4fc59a91d3a8
Mohamed Ouedarbi <mouedarbi@gmail.com>
2026-01-27 08:38:59 +0100

    feat(frontend): ajout de la liste des lieux via l'API et correction des imports backend

--- catalogue/views/__init__.py
+++ catalogue/views/__init__.py
@@ -2,3 +2,7 @@ from .artist import *
 from .type import *
 from .price import *
 from .locality import *
+from .location import *
+from .representation import *
+from .review import *
+from .show_ import *
