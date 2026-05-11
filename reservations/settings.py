5f4859f68dcc4c193ade1527663e2b286731d56e
Mpindu Mukandila Jean-Paul <jpmpindu@gmail.com>
2026-05-11 11:15:14 +0200

    rectification fichier settings.py

--- reservations/settings.py
+++ reservations/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
